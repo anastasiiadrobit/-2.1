d31de88a661dd4217fb1ecc6809ff5149e82dbac
Anastasiia Drobit <afs0030334@agderskolen.no>
2025-09-18 08:49:41 +0200

    HW1

---
+++ lesson2.1.py
@@ -0,0 +1,14 @@
+number = int(input("Enter a 4-digit number:"))
+
+
+num1 = number // 1000
+num2 =  number // 100 % 10
+num3 = number // 10 % 10
+num4 = number % 10
+
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+
