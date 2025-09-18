1952f211a3afe9f7beda8461a5d47bced3b4d3c8
Anastasiia Drobit <afs0030334@agderskolen.no>
2025-09-18 09:38:27 +0200

    HW2.1

---
+++ homework2.2.py
@@ -0,0 +1,13 @@
+num = int(input("Enter 5-digit number: "))
+
+
+d1 = num % 10
+d2 = (num // 10) % 10
+d3 = (num // 100) % 10
+d4 = (num // 1000) % 10
+d5 = (num // 10000) % 10
+
+
+
+
+print (d1,d2,d3,d4,d5)
