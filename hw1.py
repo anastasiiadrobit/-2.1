3685762149b12589f248dbcbfbbe9284542043ac
Anastasiia Drobit <afs0030334@agderskolen.no>
2025-09-22 10:40:57 +0200

    homework2.2.py

--- hw1.py
+++ hw1.py
@@ -1,16 +1,2 @@
-x = float(input("First number: "))
-op = input("Operation (+, -, *, /): ")
-y = float(input("Second number: "))
 
-if op == "+":
-    print("Result:", x + y)
-elif op == "-":
-    print("Result:", x - y)
-elif op == "*":
-    print("Result:", x * y)
-elif op == "/":
-    if x == 0:
-        print("Error:Cannot divide by zero!")
-    else:
-        print("Result:", x / y)
 
