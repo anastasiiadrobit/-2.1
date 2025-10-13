b7f0002904383c4a656b1a9dfb83baa2301262ae
Anastasiia Drobit <afs0030334@agderskolen.no>
2025-10-13 23:54:33 +0200

    homework2.2.py

--- hw3.3.py
+++ hw3.3.py
@@ -1,8 +1,5 @@
 lst = [1, 2, 3, 4, 5]
 
-if len(lst) == 0:
-    result = [[], []]
-else:
-    result = [lst[:(len(lst) + 1) // 2], lst[(len(lst) + 1) // 2:]]
+result = [lst[:(len(lst) + 1) // 2], lst[(len(lst) + 1) // 2:]] if lst else [[], []]
 
 print(result)
