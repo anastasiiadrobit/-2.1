b9f0533ff36c83b4776dd4d3f513b0f15b55285c
Anastasiia Drobit <afs0030334@agderskolen.no>
2025-10-20 00:28:53 +0200

    homework2.2.py

--- hw7.3.py
+++ hw7.3.py
@@ -1,14 +1,16 @@
 def second_index(text, some_str):
-    first_index = text.find(some_str)
-    if first_index == -1:
-        return None
-    second_index = text.find(some_str, first_index + 1)
-    if second_index == -1:
-        return None
-    return second_index
+    count = 0
+
+    for i in range(len(text) - len(some_str) + 1):
+        if text[i:i+len(some_str)] == some_str:
+            count += 1
+            if count == 2:
+                return i
+    return None
 
 assert second_index("sims", "s") == 3, 'Test1'
 assert second_index("find the river", "e") == 12, 'Test2'
 assert second_index("hi", "h") is None, 'Test3'
 assert second_index("Hello, hello", "lo") == 10, 'Test4'
 print('ОК')
+
