69101cc0e3d25f1582a025b3b4756d6bbe9ca6b4
Anastasiia Drobit <afs0030334@agderskolen.no>
2025-10-20 20:59:48 +0200

    homework2.2.py

--- hw8.2.py
+++ hw8.2.py
@@ -2,9 +2,10 @@ def is_palindrome(text):
     filtered = ''.join(char.lower() for char in text if char.isalnum())
     return filtered == filtered[::-1]
 
-assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
-assert is_palindrome('0P') == False, 'Test2'
-assert is_palindrome('a.') == True, 'Test3'
-assert is_palindrome('aurora') == False, 'Test4'
+
+assert is_palindrome('A man, a plan, a canal: Panama') is True, 'Test1'
+assert is_palindrome('0P') is False, 'Test2'
+assert is_palindrome('a.') is True, 'Test3'
+assert is_palindrome('aurora') is False, 'Test4'
 
 print("ОК")
