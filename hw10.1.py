161498510320dc90d9681c83eb407d7f0b138f36
Anastasiia Drobit <afs0030334@agderskolen.no>
2025-10-29 10:37:05 +0100

    homework2.2.py

--- hw10.1.py
+++ hw10.1.py
@@ -2,7 +2,6 @@ def is_even(digit):
     """Перевірка чи є число парним"""
     return digit % 2 == 0
 
-
 assert is_even(2) == True, 'Test1'
 assert is_even(5) == False, 'Test2'
 assert is_even(0) == True, 'Test3'
